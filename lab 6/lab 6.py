d29dfac7afac6e65c867064200d1a6bfa5ded793
Platonov A.V <74860073+antony-vipplatonov@users.noreply.github.com>
2022-12-24 17:56:20 +0300

    Changed lab

--- lab 6/lab 6.py
+++ lab 6/lab 6.py
@@ -1,15 +1,17 @@
 import telebot
 from telebot import types
-bot = telebot.TeleBot('5989845446:AAE1QpPlNdIIjqvoHp6XwLWCh8vmhLBU2-w')
-#можно было бы использовать более сложный способ хранения информации типа бд, да и этот способ не подходит,
-#если ботом пользуются несколько пользователей, но задача написать бота, так что...
+bot = telebot.TeleBot('"Ваш токен"')
+# можно было бы использовать более сложный способ хранения информации типа бд, да и этот способ не подходит,
+# если ботом пользуются несколько пользователей, но задача написать бота, так что...
 firNum = 0
 func = ""
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Введите первое число:')
     bot.register_next_step_handler(message, first)
-    
+
 
 @bot.message_handler(content_types=['button'])
 def first(message):
@@ -20,17 +22,19 @@ def first(message):
         bot.send_message(message.chat.id, 'Введите первое число:')
         bot.register_next_step_handler(message, start_message)
     markup = telebot.types.ReplyKeyboardMarkup()
-    markup.add('+', '-','*')
-    markup.add('/', '%','//')
-    bot.send_message(message.chat.id,'Выберите операцию:',reply_markup=markup)
+    markup.add('+', '-', '*')
+    markup.add('/', '%', '//')
+    bot.send_message(message.chat.id, 'Выберите операцию:', reply_markup=markup)
     bot.register_next_step_handler(message, funct)
+
 
 @bot.message_handler(commands=['text'])
 def funct(message):
     global func
     func = message.text
-    bot.send_message(message.chat.id, 'Введите второе число:',reply_markup=types.ReplyKeyboardRemove())
+    bot.send_message(message.chat.id, 'Введите второе число:', reply_markup=types.ReplyKeyboardRemove())
     bot.register_next_step_handler(message, second)
+
 
 def second(message):
     global firNum, func
@@ -39,7 +43,9 @@ def second(message):
     except:
         bot.send_message(message.chat.id, 'Введите второе число:')
         bot.register_next_step_handler(message, second)
-    bot.send_message(message.chat.id, 'Ответ: '+str(eval(str(firNum)+func+message.text)))
+    bot.send_message(message.chat.id, 'Ответ: ' + str(eval(str(firNum) + func + message.text)))
     bot.send_message(message.chat.id, 'Введите первое число:')
     bot.register_next_step_handler(message, first)
+
+
 bot.polling()
